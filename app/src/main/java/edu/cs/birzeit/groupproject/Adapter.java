a89ff3ca41e6bee26c62e65954266472db9bffb5
AdelEwaida <adel.ewaida99@gmail.com>
2020-12-12 22:46:37 +0200

    first commit by Adel

---
+++ app/src/main/java/edu/cs/birzeit/groupproject/Adapter.java
@@ -0,0 +1,67 @@
+package edu.cs.birzeit.groupproject;
+
+import android.graphics.drawable.Drawable;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter
+        extends RecyclerView.Adapter<Adapter.ViewHolder>{
+
+    private String[] captions;
+    private int[] imageIds;
+
+    public Adapter(String[] captions, int[] imageIds){
+        this.captions = captions;
+        this.imageIds = imageIds;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardView v = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.card_image, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        CardView cardView = holder.cardView;
+        ImageView imageView = (ImageView) cardView.findViewById(R.id.image);
+        Drawable dr = ContextCompat.getDrawable(cardView.getContext(), imageIds[position]);
+
+        imageView.setImageDrawable(dr);
+
+        TextView txt = (TextView)cardView.findViewById(R.id.txtName);
+        txt.setText(captions[position]);
+
+        cardView.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return captions.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private CardView cardView;
+
+        public ViewHolder(CardView cardView){
+            super(cardView);
+            this.cardView = cardView;
+        }
+
+    }
+}
---
+++ app/src/main/java/edu/cs/birzeit/groupproject/MainActivity.java
@@ -0,0 +1,31 @@
+package edu.cs.birzeit.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import edu.cs.birzeit.groupproject.model.Movie;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //test comment
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recycler = (RecyclerView)findViewById(R.id.movies_recycle);
+
+        String[] captions = new String[Movie.list.length];
+        int[] ids = new int[Movie.list.length];
+
+        for(int i = 0; i<captions.length;i++){
+            captions[i] = Movie.list[i].getTittle();
+            ids[i] = Movie.list[i].getImageId();
+        }
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        Adapter adapter = new Adapter(captions, ids);
+        recycler.setAdapter(adapter);
+    }
+
+}
