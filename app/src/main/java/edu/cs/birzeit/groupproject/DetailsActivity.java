47942507dcc23afe89284781eb499fa9e45b991b
AdelEwaida <adel.ewaida99@gmail.com>
2020-12-19 20:34:53 +0200

    Second commit by Adel

--- app/src/main/java/edu/cs/birzeit/groupproject/DetailsActivity.java
+++ app/src/main/java/edu/cs/birzeit/groupproject/DetailsActivity.java
@@ -1,12 +1,8 @@
 package edu.cs.birzeit.groupproject;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,35 +24,29 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.card_details);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         Intent intent = getIntent();
-
         // get the Movie index to show it's details
         index = intent.getExtras().getInt("index");
         System.out.println(index);
-
         movie = MainActivity.movies.get(index);
-
         img = findViewById(R.id.imgD);
         title = findViewById(R.id.nameD);
         description = findViewById(R.id.descriptionD);
         genre = findViewById(R.id.genreD);
         year = findViewById(R.id.yearD);
-
         displayData(movie);
     }
 
     private void displayData(Movie movie) {
         Drawable dr = ContextCompat.getDrawable(this, movie.getImageId());
         img.setImageDrawable(dr);
-
         title.setText(movie.getTitle());
         description.setText(movie.getDescription());
         genre.setText(movie.getGenre());
         year.setText(movie.getYear());
     }
+
 
 
 }
